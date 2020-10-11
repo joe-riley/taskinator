01131c5889e26a37ca2c0e50b07a1fa007eb19d2
Joe Riley <joeyriley@gmail.com>
2020-10-11 15:45:13 -0700

    Wrap create task in a function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
+
 var buttonE1 = document.querySelector('#save-task');
 var taskToDoE1 = document.querySelector('#tasks-to-do');
 
-buttonE1.addEventListener("click", () => {
+var createTaskHandler = () => {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     taskToDoE1.appendChild(listItemEl);
-});
+}
+
+buttonE1.addEventListener("click", createTaskHandler);
