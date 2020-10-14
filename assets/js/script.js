ef689eaaea6846b0d92184d8d9d06e082e511e71
Joe Riley <joeyriley@gmail.com>
2020-10-14 16:37:47 -0700

    Can sort of edit a task but not overwrite the old one

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+var pageContentEl = document.querySelector('.page-content');
 var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
+var taskIdCounter = 0;
 
 var createTaskHandler = (event) => {
     event.preventDefault();
@@ -20,8 +22,7 @@ var createTaskHandler = (event) => {
     }
 
     createTaskEl(taskDataObj);
-
-}
+};
 
 var createTaskEl = (taskDataObj) => {
     var listItemEl = document.createElement('li');
@@ -30,11 +31,87 @@ var createTaskEl = (taskDataObj) => {
     var taskInfoEl = document.createElement('div');
     taskInfoEl.className = 'task-info';
     taskInfoEl.innerHTML = `<h3 class="task-name">${taskDataObj.name}</h3><span class="task-type">${taskDataObj.task}</span>`;
+    
+    var id = taskIdCounter += 1;
+    listItemEl.setAttribute('data-task-id', id);
 
     listItemEl.appendChild(taskInfoEl);
 
     tasksToDoEl.appendChild(listItemEl);
 
+    taskInfoEl.appendChild(createTaskActions(id));
+
+};
+
+var createTaskActions = (taskId) => {
+    var actionContainerEl = document.createElement('div');
+    actionContainerEl.className = 'task-actions';
+
+    // create edit button
+    var editButtonEl = document.createElement('button');
+    editButtonEl.textContent = 'Edit';
+    editButtonEl.className = 'btn edit-btn';
+    editButtonEl.setAttribute('data-task-id', taskId);
+
+    actionContainerEl.appendChild(editButtonEl);
+
+    // create delete button
+    var deleteButtonEl = document.createElement('button');
+    deleteButtonEl.textContent = 'Delete';
+    deleteButtonEl.className = 'btn delete-btn';
+    deleteButtonEl.setAttribute('data-task-id', taskId);
+
+    actionContainerEl.appendChild(deleteButtonEl);
+
+    // create the status dropdown
+    var statusSelectEl = document.createElement('select');
+    statusSelectEl.className = 'select-status';
+    statusSelectEl.setAttribute('name', 'status-change');
+    statusSelectEl.setAttribute('data-task-id', taskId);
+
+    var statusChoices = ['To Do', 'In Progress', 'Completed'];
+
+    for (var i = 0; i < statusChoices.length; i++) {
+        // create the option element
+        var statusOptionEl = document.createElement('option');
+        statusOptionEl.textContent = statusChoices[i];
+        statusOptionEl.setAttribute('value', statusChoices[i]);
+
+        statusSelectEl.appendChild(statusOptionEl);
+    }
+
+    actionContainerEl.appendChild(statusSelectEl);
+
+    return actionContainerEl;
+};
+
+var deleteTask = (taskId) => {
+    var taskSelected = document.querySelector(`.task-item[data-task-id="${taskId}"]`);
+    taskSelected.remove();
 }
 
-formEl.addEventListener("submit", createTaskHandler);
+var editTask = (taskId) => {
+    var taskSelected = document.querySelector(`.task-item[data-task-id="${taskId}"]`);
+    var taskName = taskSelected.querySelector('h3.task-name').textContent;
+    document.querySelector('input[name="task-name"]').value = taskName;
+    var taskType = taskSelected.querySelector('span.task-type').textContent;
+    document.querySelector('select[name="task-type"]').value = taskType;
+
+    document.querySelector('#save-task').textContent = "Save Task";
+
+    formEl.setAttribute('data-task-id', taskId);
+}
+
+var taskButtonHandler = (event) => {
+    var taskId = event.target.getAttribute('data-task-id');
+
+    if (event.target.matches('.delete-btn')) {
+        deleteTask(taskId);
+    } else if (event.target.matches('.edit-btn')) {
+        editTask(taskId);
+    }
+};
+
+
+formEl.addEventListener('submit', createTaskHandler);
+pageContentEl.addEventListener('click', taskButtonHandler);
